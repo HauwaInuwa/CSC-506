f011906e5d36a263a9c892c81146c71a031630e8
Nakash Wali <nakashwali@gmail.com>
2021-06-05 21:06:39 +0100

    colours

--- lib/profile.dart
+++ lib/profile.dart
@@ -6,7 +6,7 @@ class ProfileApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title:Text("Profile"),backgroundColor: Colors.deepOrange,),
+        appBar: AppBar(title:Text("Profile"),backgroundColor: Colors.blueAccent,),
         body: Column(
           children: [
             Container(
@@ -14,7 +14,7 @@ class ProfileApp extends StatelessWidget {
                     gradient: LinearGradient(
                         begin: Alignment.topCenter,
                         end: Alignment.bottomCenter,
-                        colors: [Colors.deepOrange, Colors.pinkAccent]
+                        colors: [Colors.blue, Colors.blueGrey]
                     )
                 ),
 
@@ -81,7 +81,7 @@ class ProfileApp extends StatelessWidget {
                                         "CS department MAU Yola",
                                         style: TextStyle(
                                           fontSize: 20.0,
-                                          color: Colors.pinkAccent,
+                                          color: Colors.black,
                                         ),
                                       )
                                     ],
@@ -106,7 +106,7 @@ class ProfileApp extends StatelessWidget {
                                         "nakashwali@gmail.com",
                                         style: TextStyle(
                                           fontSize: 20.0,
-                                          color: Colors.pinkAccent,
+                                          color: Colors.black,
                                         ),
                                       )
                                     ],
@@ -131,7 +131,7 @@ class ProfileApp extends StatelessWidget {
                                         "08133334580",
                                         style: TextStyle(
                                           fontSize: 20.0,
-                                          color: Colors.pinkAccent,
+                                          color: Colors.black,
                                         ),
                                       )
                                     ],
@@ -148,7 +148,72 @@ class ProfileApp extends StatelessWidget {
                       
                         )
                         
-                )
+                ),
+
+                Container(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 30.0,horizontal: 16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Bio",
+                      style: TextStyle(
+                          color: Colors.redAccent,
+                          fontStyle: FontStyle.normal,
+                          fontSize: 28.0
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Text('My name is Nakashiru wali, I am  final year student in the\n'
+                        'department of computer science of Modibbo Adama University Yola',
+                      style: TextStyle(
+                        fontSize: 22.0,
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.black,
+                        letterSpacing: 2.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+             SizedBox(
+              height: 20.0,
+            ),
+            Container(
+              width: 300.00,
+            
+
+              child: TextButton(
+                  onPressed: (){},
+
+                
+
+                 child: Text('LOGOUT'),
+
+                
+                
+  
+                  /*
+                  child: Ink(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          begin: Alignment.centerRight,
+                          end: Alignment.centerLeft,
+                          colors: [Colors.pink,Colors.pinkAccent]
+                      ),
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                   
+                    ), */
+                  )
+              ),
                 ]
                 )
                 )
