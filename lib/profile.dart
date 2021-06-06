d455ce8c12d1a3d171204283c827d0c905154b7d
Hauwa Inuwa <inuwawalida@gmail.com>
2021-06-06 06:43:52 +0100

    removed banner

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,206 +1,186 @@
 import 'package:flutter/material.dart';
 
-
 class ProfileApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title:Text("Profile"),backgroundColor: Colors.blueAccent,),
-        body: Column(
-          children: [
-            Container(
-                decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [Colors.blue, Colors.blueGrey]
-                    )
-                ),
-
-                 child: Container(
-                  width: double.infinity,
-                  height: 350.0,
-                  child: Center(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        CircleAvatar(
-                         /*  backgroundImage: NetworkImage(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("Profile"),
+              backgroundColor: Colors.blueAccent,
+            ),
+            body: Column(children: [
+              Container(
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [Colors.blue, Colors.blueGrey])),
+                  child: Container(
+                      width: double.infinity,
+                      height: 350.0,
+                      child: Center(
+                          child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                            CircleAvatar(
+                              /*  backgroundImage: NetworkImage(
                             "https://www.trendrr.net/wp-content/uploads/2017/06/Deepika-Padukone-1.jpg",
                           ), */
-                          child: Icon(
-                            Icons.person,
-                            size: 60,
-                            color: Colors.blue,
-                            
-                          ),
-                          radius: 50.0,
-                          backgroundColor: Colors.white54,
-                        ),
-                        SizedBox(
-                          height: 10.0,
-                        ),
-                  Text(
-                          "Nakashiru Wali",
-                          style: TextStyle(
-                            fontSize: 22.0,
-                            color: Colors.white,
-                          ),
-                        ),
-                          SizedBox(
-                          height: 10.0,
-                        ), 
-
-                         Card(
-                          margin: EdgeInsets.symmetric(horizontal: 20.0,vertical: 5.0),
-                          clipBehavior: Clip.antiAlias,
-                          color: Colors.white,
-                          elevation: 5.0,
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 8.0,vertical: 22.0),
-                            child: Row(
-                              children: [
-                                Expanded(
-                                  child: Column(
-
-                                    children: [
-                                      Text(
-                                        "Address",
-                                        style: TextStyle(
-                                          color: Colors.redAccent,
-                                          fontSize: 22.0,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                      SizedBox(
-                                        height: 5.0,
-                                      ),
-                                      Text(
-                                        "CS department MAU Yola",
-                                        style: TextStyle(
-                                          fontSize: 20.0,
-                                          color: Colors.black,
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                ),
-                                Expanded(
-                                  child: Column(
-
-                                    children: [
-                                      Text(
-                                        "Email",
-                                        style: TextStyle(
-                                          color: Colors.redAccent,
-                                          fontSize: 22.0,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                      SizedBox(
-                                        height: 5.0,
-                                      ),
-                                      Text(
-                                        "nakashwali@gmail.com",
-                                        style: TextStyle(
-                                          fontSize: 20.0,
-                                          color: Colors.black,
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                ),
-                                Expanded(
-                                  child: Column(
-
-                                    children: [
-                                      Text(
-                                        "Phone Number",
-                                        style: TextStyle(
-                                          color: Colors.redAccent,
-                                          fontSize: 22.0,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                      SizedBox(
-                                        height: 5.0,
-                                      ),
-                                      Text(
-                                        "08133334580",
-                                        style: TextStyle(
-                                          fontSize: 20.0,
-                                          color: Colors.black,
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                ),
-                              ],
+                              child: Icon(
+                                Icons.person,
+                                size: 60,
+                                color: Colors.blue,
+                              ),
+                              radius: 50.0,
+                              backgroundColor: Colors.white54,
                             ),
-                          ),
-                        )
-                      
-                      ]
-                      )
-                        )
-                      
-                        )
-                        
-                ),
-
-                Container(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 30.0,horizontal: 16.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Bio",
-                      style: TextStyle(
-                          color: Colors.redAccent,
-                          fontStyle: FontStyle.normal,
-                          fontSize: 28.0
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                            Text(
+                              "Nakashiru Wali",
+                              style: TextStyle(
+                                fontSize: 22.0,
+                                color: Colors.white,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                            Card(
+                              margin: EdgeInsets.symmetric(
+                                  horizontal: 20.0, vertical: 5.0),
+                              clipBehavior: Clip.antiAlias,
+                              color: Colors.white,
+                              elevation: 5.0,
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 8.0, vertical: 22.0),
+                                child: Row(
+                                  children: [
+                                    Expanded(
+                                      child: Column(
+                                        children: [
+                                          Text(
+                                            "Address",
+                                            style: TextStyle(
+                                              color: Colors.redAccent,
+                                              fontSize: 22.0,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                          SizedBox(
+                                            height: 5.0,
+                                          ),
+                                          Text(
+                                            "CS department MAU Yola",
+                                            style: TextStyle(
+                                              fontSize: 20.0,
+                                              color: Colors.black,
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                    Expanded(
+                                      child: Column(
+                                        children: [
+                                          Text(
+                                            "Email",
+                                            style: TextStyle(
+                                              color: Colors.redAccent,
+                                              fontSize: 22.0,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                          SizedBox(
+                                            height: 5.0,
+                                          ),
+                                          Text(
+                                            "nakashwali@gmail.com",
+                                            style: TextStyle(
+                                              fontSize: 20.0,
+                                              color: Colors.black,
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                    Expanded(
+                                      child: Column(
+                                        children: [
+                                          Text(
+                                            "Phone Number",
+                                            style: TextStyle(
+                                              color: Colors.redAccent,
+                                              fontSize: 22.0,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                          SizedBox(
+                                            height: 5.0,
+                                          ),
+                                          Text(
+                                            "08133334580",
+                                            style: TextStyle(
+                                              fontSize: 20.0,
+                                              color: Colors.black,
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            )
+                          ])))),
+              Container(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 30.0, horizontal: 16.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Bio",
+                        style: TextStyle(
+                            color: Colors.redAccent,
+                            fontStyle: FontStyle.normal,
+                            fontSize: 28.0),
                       ),
-                    ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    Text('My name is Nakashiru wali, I am  final year student in the\n'
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Text(
+                        'My name is Nakashiru wali, I am  final year student in the\n'
                         'department of computer science of Modibbo Adama University Yola',
-                      style: TextStyle(
-                        fontSize: 22.0,
-                        fontStyle: FontStyle.italic,
-                        fontWeight: FontWeight.w300,
-                        color: Colors.black,
-                        letterSpacing: 2.0,
+                        style: TextStyle(
+                          fontSize: 22.0,
+                          fontStyle: FontStyle.italic,
+                          fontWeight: FontWeight.w300,
+                          color: Colors.black,
+                          letterSpacing: 2.0,
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                  width: 300.00,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text('LOGOUT'),
 
-             SizedBox(
-              height: 20.0,
-            ),
-            Container(
-              width: 300.00,
-            
-
-              child: TextButton(
-                  onPressed: (){},
-
-                
-
-                 child: Text('LOGOUT'),
-
-                
-                
-  
-                  /*
+                    /*
                   child: Ink(
                     decoration: BoxDecoration(
                       gradient: LinearGradient(
@@ -212,11 +192,7 @@ class ProfileApp extends StatelessWidget {
                     ),
                    
                     ), */
-                  )
-              ),
-                ]
-                )
-                )
-                );
-                }
-                }
+                  )),
+            ])));
+  }
+}
